dc71bb48584badd8344d906953e470c822a376f3
kalidratorma <kalidratorma@gmail.com>
2023-03-03 11:14:54 +0300

    kubernetes work version

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+insert into currency_exchange
+(id, currency_from, currency_to, conversion_multiple, environment)
+values(10001, 'USD', 'TRY', 18.76, '');
+
+insert into currency_exchange
+(id, currency_from, currency_to, conversion_multiple, environment)
+values(10002, 'EUR', 'TRY', 19.99, '');
+
+insert into currency_exchange
+(id, currency_from, currency_to, conversion_multiple, environment)
+values(10003, 'GBP', 'TRY', 22.69, '');
